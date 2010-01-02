0a86618597b86ccf36cf692c28aca5622590bb45
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2010-01-02 12:12:12 +0800

    passing Server scenario

--- src/greenapple/couch/Database.groovy
+++ src/greenapple/couch/Database.groovy
@@ -1,7 +1,35 @@
 package greenapple.couch
 
+import org.apache.commons.codec.net.URLCodec
+import org.apache.commons.lang.time.StopWatch
+import org.apache.http.client.HttpResponseException
+
+import org.json.JSONArray
+import org.json.JSONObject
 
 class Database {
+
+    def server
+    def host
+    def name
+    def uri
+
+    def getJSONObject = CouchUtils.&getJSONObject
+    def getMap = CouchUtils.&getMap
+    def getList = CouchUtils.&getList
+    def paramify = CouchUtils.&paramifyURL
+
+    def Database(server, name) {
+        this.server = server
+        this.name = name
+        this.host = server.getURI()
+        this.uri = "${host}/${name.replaceAll("/", "%2F")}"
+    }
+
+    // Use with caution
+    def delete() {
+        HttpClient.delete("${uri}")
+    }
 	
 }
 
