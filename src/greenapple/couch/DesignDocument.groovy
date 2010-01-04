60926b0f2844a6e219f8269ad74520cbeba03897
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2010-01-04 12:32:44 +0800

    design document

---
+++ src/greenapple/couch/DesignDocument.groovy
@@ -0,0 +1,25 @@
+package greenapple.couch
+
+class DesignDocument {    
+
+    def attributes
+    def database
+
+    // Create a design document on a database
+    protected def DesignDocument(database, String name) {
+        this.database = database
+        attributes = [:]
+        attributes.put("_id", "_design/${name}")        
+    } 
+
+    protected def push() {
+        def _id = attributes.get("_id")        
+        try {
+            def res = database.get(_id)            
+        } catch (e) {            
+            return database.save(attributes)
+        }        
+    }
+	
+}
+
