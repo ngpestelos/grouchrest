51332d7f1ef04c0c3ff261333eef13a4a1e811cf
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2010-01-04 12:53:12 +0800

    put and get

--- src/greenapple/couch/DesignDocument.groovy
+++ src/greenapple/couch/DesignDocument.groovy
@@ -2,14 +2,22 @@ package greenapple.couch
 
 class DesignDocument {    
 
-    def attributes
-    def database
+    protected def attributes
+    protected def database
 
     // Create a design document on a database
     protected def DesignDocument(database, String name) {
         this.database = database
         attributes = [:]
         attributes.put("_id", "_design/${name}")        
+    }
+
+    protected def put(key, value) {
+        attributes.put(key, value)
+    }
+
+    protected def get(key) {
+        attributes.get(key)
     }
 
     protected def push() {
